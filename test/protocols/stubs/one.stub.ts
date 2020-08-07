4c955779e4d0519e72cd1ccc5448292775281097
vikramide <>
2020-08-07 19:05:03 +0530

    fix(harmony-stub) : removed unused axios

--- test/protocols/stubs/one.stub.ts
+++ test/protocols/stubs/one.stub.ts
@@ -1,7 +1,6 @@
 import * as sinon from 'sinon'
 import BigNumber from '../../../src/dependencies/src/bignumber.js-9.0.0/bignumber'
 import { HarmonyProtocol } from '../../../src'
-import axios from '../../../src/dependencies/src/axios-0.19.0/index'
 import { ProtocolHTTPStub, TestProtocolSpec } from '../implementations'
 
 export class HarmonyProtocolStub implements ProtocolHTTPStub {
